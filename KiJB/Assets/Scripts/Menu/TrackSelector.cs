8590394781495213f607a92ce77827f68aa2f1c2
Melgamente <andrebrass2007@gmail.com>
2025-06-05 14:28:26 +0100

    JANELAS, NOMES DAS PISTAS E JEDONACO JOGÁVEL

--- KiJB/Assets/Scripts/Menu/TrackSelector.cs
+++ KiJB/Assets/Scripts/Menu/TrackSelector.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TrackSelector : MonoBehaviour
 {
-    public GameObject[] tracks; // Array com os 10 tracks disponíveis
-    private int trackSelecionada = 0; // Índice do track atualmente selecionado
+    public GameObject[] tracks; // Array com os tracks disponÃ­veis
+    public TextMeshProUGUI nomeDaPistaText; // ReferÃªncia ao TextMeshPro que mostra o nome da pista
+
+    private int trackSelecionada = 0;
 
     void Start()
     {
-        // Se já tiver um track salvo no trackSelectionManager, carrega ele
         if (TrackSelectionManager.Instance != null)
         {
             trackSelecionada = TrackSelectionManager.Instance.trackSelecionada;
@@ -19,21 +21,17 @@ public class TrackSelector : MonoBehaviour
 
     public void Mudartrack(int direcao)
     {
-        // Avança (+1) ou retrocede (-1) na seleção
         trackSelecionada += direcao;
 
-        // Se passar do último, volta para o primeiro
         if (trackSelecionada >= tracks.Length)
         {
             trackSelecionada = 0;
         }
-        // Se for menor que o primeiro, volta para o último
         else if (trackSelecionada < 0)
         {
             trackSelecionada = tracks.Length - 1;
         }
 
-        // Atualiza o gerenciador global com o novo track selecionado
         if (TrackSelectionManager.Instance != null)
         {
             TrackSelectionManager.Instance.SelecionarTrack(trackSelecionada);
@@ -44,10 +42,15 @@ public class TrackSelector : MonoBehaviour
 
     void AtualizarVisualizacao()
     {
-        // Ativa apenas o track atualmente selecionado
         for (int i = 0; i < tracks.Length; i++)
         {
             tracks[i].SetActive(i == trackSelecionada);
+        }
+
+        // Atualiza o texto com o nome da pista
+        if (nomeDaPistaText != null)
+        {
+            nomeDaPistaText.text = tracks[trackSelecionada].name;
         }
     }
 }
